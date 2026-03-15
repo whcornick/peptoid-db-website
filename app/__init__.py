c5549e298cd307e2afbb620e1a7f5b58bae33000
William Cornick <whc4657@nyu.edu>
2026-03-15 21:40:05 +0000

    Update Flask-Limiter initialization for modern API

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
 bootstrap = Bootstrap(app)
-limiter = Limiter(app, key_func=get_remote_address)
+limiter = Limiter(key_func=get_remote_address, app=app)
 moment = Moment(app)
 basic_auth = BasicAuth(app)
 
