913c2b988c404435f206a9c38559ce29aa173d57
William Cornick <whc4657@nyu.edu>
2026-06-30 19:35:19 +0000

    Add live database migration compatibility

---
+++ migrations/versions/85c558bd9870_legacy_live_schema_stamp.py
@@ -0,0 +1,22 @@
+"""Legacy live schema stamp
+
+Revision ID: 85c558bd9870
+Revises:
+Create Date: 2026-06-30
+
+This revision represents the schema already present in the live database
+before the contributor/submission migration chain was added.
+"""
+
+revision = '85c558bd9870'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
